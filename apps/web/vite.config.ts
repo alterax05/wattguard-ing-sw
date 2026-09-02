d8fdfa8e53b50af0b7fdf6d77762826df5a615ac
itsPiovez <129937836+itsPiovez@users.noreply.github.com>
2026-09-02 17:30:37 +0200

    chore: upgrade vite

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,15 +1,11 @@
 import { defineConfig } from "vite";
-import react, { reactCompilerPreset } from "@vitejs/plugin-react";
-import babel from "@rolldown/plugin-babel";
+import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "node:path";
 
 export default defineConfig({
   plugins: [
-    react(),
-    babel({
-      presets: [reactCompilerPreset()],
-    }),
+    react({ compiler: true }),
     tailwindcss(),
   ],
   resolve: {
